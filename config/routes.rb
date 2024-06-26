b4c49341a1dbd2066e6f8e633f46c4ef28e0cadc
Adnir Andrade <adnir_andrade@hotmail.com>
2024-06-26 16:53:09 -0300

    Add controllers, helpers and some routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
 Rails.application.routes.draw do
+  get 'campaings/index'
+  get 'campaings/show'
+  get 'campaings/new'
+  get 'campaings/create'
+  get 'campaings/edit'
+  get 'campaings/update'
+  get 'campaings/destroy'
+  get 'characters/index'
+  get 'characters/show'
+  get 'characters/new'
+  get 'characters/create'
+  get 'characters/edit'
+  get 'characters/update'
+  get 'characters/destroy'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
