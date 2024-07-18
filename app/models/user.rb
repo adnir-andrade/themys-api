fc909363fbd08377cfb3aadc448c2f626d02a71c
Adnir Andrade <adnir_andrade@hotmail.com>
2024-07-18 14:49:42 -0300

    Fix dependencies in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many :campaigns_as_dm, class_name: 'Campaign', foreign_key: 'dm_id'
-    has_many :campaigns_players
-    has_many :campaigns_as_player, through: :campaigns_players, source: :campaign
+  has_many :campaigns_as_dm, class_name: 'Campaign', foreign_key: 'dm_id'
+  has_many :campaigns_players
+  has_many :campaign, through: :campaigns_players, source: :campaign
 end
