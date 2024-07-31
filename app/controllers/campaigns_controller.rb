6e48a124eed0b3fec60bb055b7eb1fa5fdb5349a
Adnir Andrade <adnir_andrade@hotmail.com>
2024-07-31 17:31:42 -0300

    Implement validation to check if there is a campaign

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -47,7 +47,11 @@ class CampaignsController < ApplicationController
   private
 
   def set_campaign
-    @campaign = Campaign.find(params[:id])
+    begin
+      @campaign = Campaign.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: "Campaign not found" }, status: :not_found
+    end
   end
 
   def campaign_params
