1514fc855ff058d66017fa5d61e27399269711fd
Adnir Andrade <adnir_andrade@hotmail.com>
2024-07-31 17:31:02 -0300

    Add migrations to deal with DELETE request, as campaign is a FK in characters

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,17 @@
-ActiveRecord::Schema[7.1].define(version: 2024_06_24_013918) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.1].define(version: 2024_07_31_202650) do
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "campaigns", force: :cascade do |t|
@@ -43,7 +56,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_24_013918) do
     t.integer "current_hp"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "campaigns_player_id", null: false
+    t.bigint "campaigns_player_id"
     t.index ["campaigns_player_id"], name: "index_characters_on_campaigns_player_id"
     t.index ["player_id"], name: "index_characters_on_player_id"
   end
@@ -85,7 +98,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_24_013918) do
   add_foreign_key "campaigns_players", "campaigns"
   add_foreign_key "campaigns_players", "characters"
   add_foreign_key "campaigns_players", "users", column: "player_id"
-  add_foreign_key "characters", "campaigns_players"
+  add_foreign_key "characters", "campaigns_players", on_delete: :nullify
   add_foreign_key "characters", "users", column: "player_id"
   add_foreign_key "inventories", "characters"
   add_foreign_key "items_stacks", "inventories"
