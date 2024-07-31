0dfb21e44ee530e0d9232df0327118d2ef76a7ec
Adnir Andrade <adnir_andrade@hotmail.com>
2024-07-31 13:43:34 -0300

    Fix logic in model

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,5 +1,5 @@
 class Campaign < ApplicationRecord
   belongs_to :dm, class_name: 'User', foreign_key: 'dm_id'
-  has_many :campaigns_players
-  has_many :players, through: :campaigns_players, source: :user
+  has_many :campaigns_players, dependent: :destroy
+  has_many :players, through: :campaigns_players, source: :player
 end
