f1cac9772c6aca25f56fc9231ade4abbaa532f43
Adnir Andrade <adnir_andrade@hotmail.com>
2024-09-02 19:06:39 -0300

    Remove unused reference

--- app/controllers/campaigns_report_controller.rb
+++ app/controllers/campaigns_report_controller.rb
@@ -1,8 +1,6 @@
 require_relative "../queries/campaigns/index_query"
 
 class CampaignsReportController < ReportsController
-  include CampaignQuery
-
   def index
     data = CampaignQuery::sort_data(@sorter)
     headers = ['Name', 'Next Session']
