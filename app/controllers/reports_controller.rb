96da998beb36a14012f23757ec677f145ac9f93f
Adnir Andrade <adnir_andrade@hotmail.com>
2024-09-02 19:05:08 -0300

    Implement player's character by campaign report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,3 +1,5 @@
+require_relative "../queries/campaigns_players/character_player_by_campaign"
+
 class ReportsController < ApplicationController
   before_action :set_sorter
 
@@ -6,12 +8,20 @@ class ReportsController < ApplicationController
       format.pdf do
         pdf = BasePdf.new(data: data, headers: headers, title: title)
         pdf.render_document
-        send_data(pdf.render, filename: "#{filename}.pdf", type: 'application/pdf')
+        send_data(pdf.render, filename: "#{filename}.pdf", type: 'application/pdf', disposition: "inline"
+        )
       end
     end
   end
 
   def set_sorter
     @sorter = params[:option]
+  end
+
+  def character_player_by_campaign
+    data = CharacterPlayerByCampaign::sort_data(@sorter)
+    headers = ['Username', 'Campaign', 'Character']
+    title = "Player's character by campaign"
+    generate_report("player-character-by-campaign", data, headers, title)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :campaigns
   resources :trades, only: [:create]
 
-  get '/reports/campaigns/(:option)', to: 'campaigns_report#index', defaults: { format: :pdf }
+  get '/reports/campaigns/index/(:option)', to: 'campaigns_report#index', defaults: { format: :pdf }
+  get '/reports/campaigns/characters/(:option)', to: 'reports#character_player_by_campaign', defaults: { format: :pdf }
 end
